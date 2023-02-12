63179473b4371baef2e3abe9f18e74514a034d8d
Logickin <logickinlambda@gmail.com>
2023-02-12 11:13:15 +0800

    More Documentation - Added module level description

--- src/n_log_n/mod.rs
+++ src/n_log_n/mod.rs
@@ -1,3 +1,5 @@
+//! Here are the sorting algorithms with the O(n log n) time complexity; usually, these are some of the most practical algorithms,
+//! which they offers a reasonable running time with reasonable storage size.
 pub mod quick_sort;
 pub mod merge_sort;
 
